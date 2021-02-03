388e624626092e0c9b3d3381c341e3eafa7db9b5
pollsav <59764016+pollsav@users.noreply.github.com>
2021-02-03 14:13:00 +0300

    form tasks

---
+++ form.js
@@ -0,0 +1,13 @@
+// const b1 = document.querySelector('#b-1')
+// b1.addEventListener('click', () =>{
+//     alert('task1')
+// })
+// const b2 = document.querySelector('#b-2')
+// b2.addEventListener('click', ()=>{
+//     alert('task2')
+// })
+// const p = document.querySelector('.p-3')
+// p.style.cursor = 'pointer'
+// p.addEventListener('click', ()=>{
+//     alert('task3')
+// })
