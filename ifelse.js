7c550ab41a5599bf4bf57fa6f58e07a01028defe
pollsav <59764016+pollsav@users.noreply.github.com>
2021-02-03 13:34:45 +0300

    task

---
+++ ifelse.js
@@ -0,0 +1,37 @@
+// const i1 = document.querySelector('#i-1')
+// const b1 = document.querySelector('#b-1')
+// const out1 = document.querySelector('.out-1')
+// b1.addEventListener('click',function(){
+//     if(i1.value == 4){
+//         out1.innerHTML = 'true'
+//     }else{
+//         out1.innerHTML = 'false'
+//     }
+// })
+
+// let a21 = 122
+// let a22 = 11
+// const b2 = document.querySelector('.b-2')
+// const out2 = document.querySelector('.out-2')
+
+// b2.addEventListener('click',function(){
+//     if(a21 > a22){
+//         out2.innerHTML = a21
+//     }else{
+//         out2.innerHTML = a22
+//     }
+// })
+
+// const i31 = document.querySelector('#i-31')
+// const i32 = document.querySelector('#i-32')
+// const b3 = document.querySelector('#b-3')
+// const out3 = document.querySelector('.out-3')
+// b3.addEventListener('click',function(){
+//     a = +i31.value
+//     b = +i32.value
+//     if(a > b){
+//         out3.innerHTML = a
+//     }else{
+//         out3.innerHTML = b
+//     }
+// })
