13521f226cd624db62c16ae3f41c089f16a29a5d
pollsav <59764016+pollsav@users.noreply.github.com>
2021-02-04 14:00:54 +0300

    cycle task

---
+++ cycle.js
@@ -0,0 +1,23 @@
+// const out1 = document.querySelector('.out-1')
+// const b1 = document.querySelector('#b-1')
+// b1.addEventListener('click', ()=>{
+//     for(let i = 0; i <= 50; i++){
+//         out1.innerHTML += " " + i
+//     }
+// })
+
+// const out2 = document.querySelector('.out-2')
+// const b2 = document.querySelector('#b-2')
+// b2.addEventListener('click', ()=>{
+//     for(let i = 2; i <= 122; i += 2){
+//         out2.innerHTML += " " + i
+//     }
+// })
+
+const out3 = document.querySelector('.out-3')
+const b3 = document.querySelector('#b-3')
+b3.addEventListener('click', ()=>{
+    for(let i = 25; i >= 0; i--){
+        out3.innerHTML += " " + i
+    }
+})
