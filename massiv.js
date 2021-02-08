8afedf9d58b51653ee956fb8b97ec3a2a3a7d014
pollsav <59764016+pollsav@users.noreply.github.com>
2021-02-08 22:09:27 +0300

    task

---
+++ massiv.js
@@ -0,0 +1,11 @@
+// let ar1 = [1,2,'3',true]
+
+// let b1 = document.querySelector('#b-1')
+// let out1 = document.querySelector('#out-1')
+
+// b1.addEventListener('click',()=>{
+//     for(let i = 0; i < ar1.length; i++){
+//        console.log(ar1[i])
+//        out1.innerHTML += ar1[i]  + ' '
+//     }
+// })
